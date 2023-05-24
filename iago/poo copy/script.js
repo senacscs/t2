c87ce801c907448eab2cbcf8186616d5c34fc6f7
Iag0o0 <133239546+Iag0o0@users.noreply.github.com>
2023-05-24 15:29:53 -0300

    poo

--- iago/poo copy/script.js
+++ iago/poo copy/script.js
@@ -10,8 +10,8 @@ class Animal {
     }
 
     rastejar() {
-        document.getElementById("mostrar").innerHTML = this.nome + " está rastejando";
-        console.log(`${this.nome} está rastejando`);
+        document.getElementById("show").innerHTML = this.nome + " está rastejando";
+        console.log("${this.nome} está rastejando");
     }
 }
 
@@ -45,23 +45,43 @@ class Cadeira {
     }
 }
 
+class Corda {
+    constructor(nome, patas) {
+        this.nome = nome;
+        this.patas = patas;
+    }
+
+    rastejar() {
+        document.getElementById("show").innerHTML = this.nome + " é uma corda, não pode rastejar";
+        console.log("${this.nome} é uma corda, não pode rastejar");
+    }
+}
+
 function criaObjeto() {
     let nome = document.getElementById("nomeInput").value;
     let patas = parseInt(document.getElementById("patasInput").value);
 
 
     if (patas === 0) {
-        return new Cobra(nome);
+        let respira = prompt("Respira?");
+        if (respira === "sim" || respira === "Sim" || respira === "SIM" || respira === "s" || respira === "S") {
+            return new Cobra(nome);
+        }
+        return new Corda(nome);
+
     } else if (patas === 2) {
         return new Humano(nome);
+
     } else if (patas === 4) {
         let respira = prompt("Respira?");
         if (respira === "sim" || respira === "Sim" || respira === "SIM" || respira === "s" || respira === "S") {
             return new Cachorro(nome);
         }
         return new Cadeira(nome);
+
     } else if (patas === 3) {
         return new Cadeira(nome);
+
     } else {
         return null;
     }
@@ -69,10 +89,13 @@ function criaObjeto() {
 
 function criar() {
     let obj = criaObjeto();
-    if (obj === 0) {
+    if (obj === null) {
+        console.log("Para patas é 0, 2 ou 4 apenas");
+    } else if (obj === Corda || obj === Cobra) {
         obj.rastejar();
     } else {
         obj.correr();
     }
 }
+
 
