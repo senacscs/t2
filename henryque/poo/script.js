85c15d92b07beb5ac9beacc313170aca7b2affb4
ExpertFox1234 <60172893+ExpertFox1234@users.noreply.github.com>
2023-05-31 11:10:00 -0300

    amo técnico

---
+++ henryque/poo/script.js
@@ -0,0 +1,90 @@
+class Animal {
+    constructor(nome, patas) {
+        this.nome = nome;
+        this.patas = patas;
+    }
+    
+    correr() {
+        document.getElementById("mostrar").innerHTML = this.nome + " está correndo";
+        console.log(`${this.nome} está correndo`);
+    }
+}
+
+class Cachorro extends Animal {
+    constructor(nome) {
+        super(nome, 4);
+    }
+}
+
+class Humano extends Animal {
+    constructor(nome) {
+        super(nome, 2);
+    }
+}
+
+class Inseto extends Animal {
+    constructor(nome) {
+        super(nome, 6, 8);
+    }
+}
+
+class Cadeira {
+    constructor(nome, patas) {
+        this.nome = nome;
+        this.patas = patas;
+    }
+
+    correr() {
+        document.getElementById("mostrar").innerHTML = this.nome + " é uma cadeira, não pode correr";
+        console.log("${this.nome} é uma cadeira, não pode correr");
+    }
+}
+
+class Mesa {
+    constructor(nome, patas) {
+        this.nome = nome;
+        this.patas = patas;
+    }
+
+    correr() {
+        document.getElementById("mostrar").innerHTML = this.nome + " é uma FODENDO MESA DE JANTAR, não pode correr (infelizmente)";
+        console.log("${this.nome} é uma mesa,  felizmente ou infelizmente não pode correr");
+    }
+}
+
+function criaObjeto() {
+    let nome = document.getElementById("nomeInput").value;
+    let patas = parseInt(document.getElementById("patasInput").value);
+
+    if (patas === 2) {
+        return new Humano(nome);
+    } else if (patas === 4) {
+        let respira = prompt("Respira?");
+        if (respira === "sim" || respira === "Sim" || respira === "SIM" || respira === "s" || respira === "S") {
+            return new Cachorro(nome);
+        }
+        return new Cadeira(nome);
+    } else if (patas === 3) {
+        return new Cadeira(nome);
+    } else if (patas >= 6 && patas <= 8){
+        let respira = prompt("Respira?");
+        if (respira === "sim" || respira === "Sim" || respira === "SIM" || respira === "s" || respira === "S") {
+            return new Inseto(nome);
+        }
+        return new Cadeira(nome);
+    } else  if( patas > 8){
+        return new Mesa(nome);
+    } else {
+        return null
+    }
+}
+
+function criar() {
+    let obj = criaObjeto();
+    if (obj === null) {
+        console.log("Para patas é 2 ou 4 apenas");
+    } else {
+        obj.correr();
+    }
+}
+
