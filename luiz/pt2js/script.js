81684049f5766020084088912315c20cb94023c3
Luiz Henrique Rauber Rodrigues <133702602+luizrauber@users.noreply.github.com>
2023-08-31 14:53:01 -0300

    pt 2 js

---
+++ luiz/pt2js/script.js
@@ -0,0 +1,12 @@
+
+
+//ola mundo
+function olaMundo() {
+    document.getElementById("mostrar").innerHTML = "Olá Mundo!";
+    console.log(`Olá Mundo!`);
+    alert(`Olá Mundo!`);
+}
+
+function numeroDigitado() {
+    document.getElementById("mostrar").innerHTML = "O número digitado foi " + numero.value;
+}
