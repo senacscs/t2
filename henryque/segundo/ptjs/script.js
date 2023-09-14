6b5100449a5aab564abe951f444d8e1c88f70903
ExpertFox1234 <60172893+ExpertFox1234@users.noreply.github.com>
2023-09-14 10:37:37 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- henryque/segundo/ptjs/script.js
+++ henryque/segundo/ptjs/script.js
@@ -11,3 +11,16 @@ function nomeDigitado(){
     var nome = document.getElementById("nome");
     alert("O nome digitado foi: " + nome + "!")
 }
+
+function operacoes(){
+    var num1 = parseFloat(document.getElementById("a").value);
+    var num2 = parseFloat(document.getElementById("b").value);
+
+    const soma = num1 + num2
+    const div = num1 / num2
+    const mult = num1 * num2
+    const sub = num1 - num2
+
+    alert("Aplicando as 4 operações os resultados seriam: " + "\nSoma: " + soma + "\nDivisão: " + div + "\nMultiplicação: " + mult + "\nSubtração: " + sub)
+
+}
