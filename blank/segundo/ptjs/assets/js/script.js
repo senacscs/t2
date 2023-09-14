24d6a64955c72afc9d16b22da3e22754d7382efd
gigi <105399656+giiovanaa@users.noreply.github.com>
2023-09-14 10:33:49 -0300

    att

--- blank/segundo/ptjs/assets/js/script.js
+++ blank/segundo/ptjs/assets/js/script.js
@@ -1,36 +1,74 @@
 //ola mundo
 function olaMundo() {
-    document.getElementById("mostrar").innerHTML = "Olá Mundo!";
-    console.log('Olá Mundo!');
-    alert('Olá Mundo!');
+  document
+      .getElementById("mostrar")
+      .innerHTML = "Olá Mundo!";
 }
 
-
+//numero digitado
 function numeroDigitado() {
 
-  document.getElementById("output").innerHTML = "O número digitado foi " + numero.value;
+  document
+      .getElementById("outputNum")
+      .innerHTML = "O número digitado foi " + numero.value;
 }
 
-
+//seu nome
 function seuNome() {
-  var name = prompt("Digite seu nome: ");
-  document.getElementById("output").innerHTML = "O nome digitado foi: " + name;
+  document
+      .getElementById("outputName")
+      .innerHTML = "O nome digitado foi: " + nome.value;
 }
 
+// MODAIS 
+// modal olá mundo
+var modalHello = document.getElementById("hello");
+var btnHello = document.getElementById("olaMundoModal");
+var spanHelloClose = document.getElementsByClassName("close")[0];
+btnHello.onclick = function () {
+  modalHello.style.display = "block";
 
-// modal
-var modal = document.getElementById("num");
-var btn = document.getElementById("numModal");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-  modal.style.display = "block";
-
-  span.onclick = function() {
-    modal.style.display = "none";
+  spanHelloClose.onclick = function () {
+      modalHello.style.display = "none";
   }
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
+  window.onclick = function (event) {
+      if (event.target == modalHello) {
+          modalHello.style.display = "none";
+      }
   }
 }
+
+//modal numero digitado
+var modalNum = document.getElementById("num");
+var btnNum = document.getElementById("numModal");
+var spanNumClose = document.getElementsByClassName("close")[0];
+btnNum.onclick = function () {
+  modalNum.style.display = "block";
+
+  spanNumClose.onclick = function () {
+      modalNum.style.display = "none";
+  }
+  window.onclick = function (event) {
+      if (event.target == modalNum) {
+          modalNum.style.display = "none";
+      }
+  }
+}
+
+// modal seu nome
+var modalName = document.getElementById("name");
+var btnName = document.getElementById("nameModal");
+var spanName = document.getElementsByClassName("close")[0];
+btnName.onclick = function () {
+  modalName.style.display = "block";
+
+  spanName.onclick = function () {
+      modalName.style.display = "none";
+  }
+  window.onclick = function (event) {
+      if (event.target == modalName) {
+          modalName.style.display = "none";
+      }
+  }
+}
+
