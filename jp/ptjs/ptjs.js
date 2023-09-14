91e7a5f38cc782dd08d466ed6e034cd8e7232f0d
jpbari <133238475+jpbari@users.noreply.github.com>
2023-09-14 10:52:10 -0300

    ptjs

---
+++ jp/ptjs/ptjs.js
@@ -0,0 +1,14 @@
+function olaMundo() {
+    document.getElementById("mostrar").innerHTML = "Olá Mundo!";
+    console.log(`Olá Mundo!`);
+    alert(`Olá Mundo!`);
+}
+
+function numeroDigitado() {
+    document.getElementById("mostrar").innerHTML = "O número digitado foi " + numero.value;
+}
+
+function nomeDigitado() {
+    document.getElementById("mostrar").innerHTML = "O nome digitado é"
+    
+}
