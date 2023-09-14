3e2488799c45b5c5b68abae410a9adef6b3a6a69
manuelatrevisan <133238952+manuelatrevisan@users.noreply.github.com>
2023-09-14 10:01:42 -0300

    Subindo Portugol para JS

---
+++ trevisan/segundo/ptjs/script.js
@@ -0,0 +1,14 @@
+//ola mundo 
+function olaMundo() {
+    document.getElementById("mostrar").innerHTML = "Olá Mundo!"; 
+    console.log('Olá Mundo!');
+    alert('Olá Mundo!'); 
+}
+
+function numeroDigitado() {
+    document.getElementById("mostrar").innerHTML = "O nome número digitado foi " + number.value;
+ }
+
+ function nomeDigitado() {
+    document.getElementById("mostrar").innerHTML = "O nome digitado foi " +  nome.value;
+ }
