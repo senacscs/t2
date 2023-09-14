5a25400638694207472fca643d9d8fec11c11d27
gigi <105399656+giiovanaa@users.noreply.github.com>
2023-09-14 10:02:57 -0300

    atualizações

--- blank/segundo/ptjs/assets/js/script.js
+++ blank/segundo/ptjs/assets/js/script.js
@@ -4,3 +4,33 @@ function olaMundo() {
     console.log('Olá Mundo!');
     alert('Olá Mundo!');
 }
+
+
+function numeroDigitado() {
+
+  document.getElementById("output").innerHTML = "O número digitado foi " + numero.value;
+}
+
+
+function seuNome() {
+  var name = prompt("Digite seu nome: ");
+  document.getElementById("output").innerHTML = "O nome digitado foi: " + name;
+}
+
+
+// modal
+var modal = document.getElementById("num");
+var btn = document.getElementById("numModal");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
