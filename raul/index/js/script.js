ece1a2afd4fa9c963d12f3f99cb7497ea1039678
raulflores5 <133238940+raulflores5@users.noreply.github.com>
2023-12-06 11:07:25 -0300

    .

---
+++ raul/index/js/script.js
@@ -0,0 +1,23 @@
+$(function () {
+
+
+    //HEADER
+    $(window).scroll(function () {
+          if($(this).scrollTop() > 200)
+          {
+              if (!$('.main_header').hasClass('fixed'))
+              {
+                  $('.main_header').stop().addClass('fixed').css('top', '-100px').animate(
+                      {
+                          'top': '0px'
+                      }, 500);
+              }
+          }
+          else
+          {
+              $('.main_header').removeClass('fixed');
+          }
+    });
+
+
+});
