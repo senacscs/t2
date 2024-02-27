84ed8a944def620791e9d080a5d0d54f445be0f5
mzlguto <133239192+mzlguto@users.noreply.github.com>
2024-02-27 07:55:13 -0300

    t

--- augusto/Terceiro/indicacoesmobile/assets/js/mobile-navbar.js
+++ augusto/Terceiro/indicacoesmobile/assets/js/mobile-navbar.js
@@ -55,11 +55,6 @@ const mobileNavbar = new MobileNavbar(
 });
 
 
-window.addEventListener('resize', function() {
-  if(window.outerWidth < 500) {
-      window.resizeTo(500, window.outerHeight);
-  }
-}, true)
 
 
 window.onscroll = function() {scrollFunction()};
