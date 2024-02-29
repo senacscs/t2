fba5f2e90a2075c54b18a0629dff3ed3bf57ad88
Octávio <112187402+ooctaviomz@users.noreply.github.com>
2024-02-29 09:48:39 -0300

    k

--- octavio/terceiro/indicacoesmobile/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const sections = document.querySelectorAll('section');
-
-  sections.forEach(section => {
-      const title = section.querySelector('h2');
-      const content = section.querySelector('p');
-
-      title.addEventListener('click', () => {
-          content.classList.toggle('hidden');
