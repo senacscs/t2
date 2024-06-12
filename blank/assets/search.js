bdeec5605d13a7933db0e5c99230645616d8a663
gigi <105399656+giiovanaa@users.noreply.github.com>
2024-06-12 12:00:09 -0300

    atualização search bar portfolio

--- blank/assets/search.js
+++ blank/assets/search.js
@@ -1,22 +1,14 @@
- //search bar
-let searchBar = document.getElementById("searchBar");
-let projects = document.querySelectorAll(".project");
-
- searchBar.addEventListener("input", function () {
-     const searchTerm = searchBar
-         .value
-         .toLowerCase();
-
-     projects.forEach(projectTag => {
-         const name = projectTag
-             .querySelector(".tags span")
-             .textContent
-             .toLowerCase();
-
-         if (name.startsWith(searchTerm)) {
-             projectTag.style.display = "block";
-         } else {
-             projectTag.style.display = "none";
-         }
-     });
- });
+function pesquisar_index() {
+    let input = document.getElementById('searchInput').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('card');
+     
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="card";                
+        }
+    }
+}
