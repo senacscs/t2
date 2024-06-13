eee73a87dd76991771eb81ebfaf18c05aba81a4f
Luiz Henrique Rauber Rodrigues <133702602+luizrauber@users.noreply.github.com>
2024-06-13 16:09:14 -0300

    pwa1

---
+++ luiz/pwa1/callsw.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', () => {
+    registerSW();
+});
+
+// Register the Service Worker
+async function registerSW() {
+    if ('serviceWorker' in navigator) {
+        try {
+            await navigator
+                .serviceWorker
+                .register('serviceworker.js');
+        }
+        catch (e) {
+            console.log('SW registration failed');
+        }
+    }
+}
