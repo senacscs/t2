1c6635654d2dad4731d5d6059d247560b441c7f3
raulflores5 <133238940+raulflores5@users.noreply.github.com>
2024-07-19 08:09:26 -0300

    mvp pp

---
+++ raul/terceiro/mvp/js/menu.js
@@ -0,0 +1,10 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
