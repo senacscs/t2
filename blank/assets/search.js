28c29927491bc1e331487992f6c61e85fbd17e1d
gigi <105399656+giiovanaa@users.noreply.github.com>
2024-08-13 12:15:53 -0300

    funcione funcione funcioneeeeeee

--- blank/assets/search.js
+++ blank/assets/search.js
@@ -1,15 +1,15 @@
-// function pesquisar_index() {
-//     let input = document.getElementById('searchInput').value
-//     input=input.toLowerCase();
-//     let x = document.getElementsByClassName('card');
+function pesquisar_index() {
+    let input = document.getElementById('searchInput').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('card');
      
-//     for (i = 0; i < x.length; i++) {
-//         if (!x[i].innerHTML.toLowerCase().includes(input)) {
-//             x[i].style.display="none";
-//         }
-//         else {
-//             x[i].style.display="card";                
-//         }
-//     }
-// }
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="card";                
+        }
+    }
+}
 
