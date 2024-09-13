fe99d068da92b1ddd823a2bc54c22da15b34098e
gigi <105399656+giiovanaa@users.noreply.github.com>
2024-09-13 08:10:47 -0300

    Correção de bugs: português e acessibilidade

---
+++ blank/assets/filterSelection.js
@@ -0,0 +1,46 @@
+
+
+// Filtra os elementos
+function filterSelection(c) {
+    var x = document.getElementsByClassName("filterDiv");
+    if (c == "all") c = "";
+    // Adiciona a classe "show" aos elementos filtrados e remove dos não selecionados
+    Array.from(x).forEach(function(element) {
+      element.classList.remove("show");
+      if (element.className.indexOf(c) > -1) {
+        element.classList.add("show");
+      }
+    });
+  }
+  
+  // Mostra os elementos filtrados
+  function w3AddClass(element, name) {
+    var arr = name.split(" ");
+    arr.forEach(function(cls) {
+      if (!element.classList.contains(cls)) {
+        element.classList.add(cls);
+      }
+    });
+  }
+  
+  // Esconde os elementos que não são selecionados
+  function w3RemoveClass(element, name) {
+    var arr = name.split(" ");
+    arr.forEach(function(cls) {
+      element.classList.remove(cls);
+    });
+  }
+  
+  // Adiciona a classe ativa ao botão atual (destaca-o)
+  var btnContainer = document.getElementById("btts");
+  var btns = btnContainer.getElementsByClassName("btn");
+  Array.from(btns).forEach(function(btn) {
+    btn.addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+      if (current.length > 0) {
+        current[0].classList.remove("active");
+      }
+      this.classList.add("active");
+    });
+  });
+  
