55816ee2182fe369296ec35fce994d98177b595d
Kassio-Alves <145720250+Kassio-Alves@users.noreply.github.com>
2024-10-08 11:43:53 -0300

    run

--- kassio/terceiro/listaingles/scriptt.js
+++ kassio/terceiro/listaingles/scriptt.js
@@ -2,7 +2,7 @@ function showTranslation(word) {
     let translationElement = document.getElementById(word + '-translation');
     if (word === 'tenis') {
         translationElement.textContent = 'Tênis - Um tipo de calçado esportivo.';
-    } else if (word === 'meias') {
-        translationElement.textContent = 'Meias - Peça de roupa usada nos pés.';
+    } else if (word === 'red') {
+        translationElement.textContent = 'Vermelho - Cor vermelha.';
     }
 }
