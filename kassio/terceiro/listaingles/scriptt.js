8d340ed466a753e95aa38817ea779807e3de5170
Kassio-Alves <145720250+Kassio-Alves@users.noreply.github.com>
2024-10-08 11:46:05 -0300

    run

--- kassio/terceiro/listaingles/scriptt.js
+++ kassio/terceiro/listaingles/scriptt.js
@@ -2,7 +2,7 @@ function showTranslation(word) {
     let translationElement = document.getElementById(word + '-translation');
     if (word === 'tenis') {
         translationElement.textContent = 'Tênis - Um tipo de calçado esportivo.';
-    } else if (word === 'red') {
+    } else if (word === 'Vermelho') {
         translationElement.textContent = 'Vermelho - Cor vermelha.';
     }
 }
