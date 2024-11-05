57df118f8fb2ccab99c518a8bd62bb1ba75a558f
Kassio-Alves <145720250+Kassio-Alves@users.noreply.github.com>
2024-11-05 11:34:09 -0300

    Run

--- kassio/terceiro/listaingles/scriptt.js
+++ kassio/terceiro/listaingles/scriptt.js
@@ -1,8 +1,8 @@
-function showTranslation(word) {
-    let translationElement = document.getElementById(word + '-translation');
-    if (word === 'tenis') {
-        translationElement.textContent = 'Tênis - Um tipo de calçado esportivo.';
-    } else if (word === 'vermelho') {
-        translationElement.textContent = 'Vermelho - Cor vermelha.';
+    function showTranslation(word) {
+        let translationElement = document.getElementById(word + '-translation');
+        if (word === 'tenis') {
+            translationElement.textContent = 'Tênis - Um tipo de calçado esportivo.';
+        } else if (word === 'vermelho') {
+            translationElement.textContent = 'Vermelho - Cor vermelha.';
+        }
     }
-}
