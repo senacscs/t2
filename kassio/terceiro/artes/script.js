eaf97b18005bfaf8761c791c2c15aeb34bc4e38e
Kassio-Alves <145720250+Kassio-Alves@users.noreply.github.com>
2024-11-26 10:56:15 -0300

    run

---
+++ kassio/terceiro/artes/script.js
@@ -0,0 +1,7 @@
+// script.js
+document.querySelectorAll('.read-more').forEach(button => {
+    button.addEventListener('click', () => {
+      alert('Mais informações em breve!');
+    });
+  });
+  
